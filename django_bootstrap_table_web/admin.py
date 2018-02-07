06fd042a14da2ec6db987df70ca9e1faa5206bc6
netbuffer <javawiki@163.com>
2018-02-07 13:37:24 +0800

    django custom admin

--- django_bootstrap_table_web/admin.py
+++ django_bootstrap_table_web/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django_bootstrap_table_web.models import User
 
+
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    # fields = ['id', 'name', "age", "sex"]
+    # 控制管理页面展示的字段
+    list_display = ('id', 'name', "age", "sex")
+    # 查询过滤器
+    list_filter = ['age']
+    # 搜索字段
+    search_fields = ['name', "sex", "age"]
+
+
 # 注册User model
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
