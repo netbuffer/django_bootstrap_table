79e8d1c4daf646acc7b2d08f077394dfbf8c3be9
netbuffer <javawiki@163.com>
2018-02-08 15:02:06 +0800

    logging

--- django_bootstrap_table/controller.py
+++ django_bootstrap_table/controller.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+import logging
+
 from django.shortcuts import render
+
+logging.basicConfig(filename='app.log', level=logging.INFO,
+                    format='%(asctime)s %(name)s %(levelname)s func:[%(funcName)s] %(message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 def index(request):
+    logger.info("index")
     # return HttpResponse("index controller")
     return render(request, "manage.html")
